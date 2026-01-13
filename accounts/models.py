48fb6cc94f9d6a8b00eb06be4f4c4d9ed0d46d54
macrespo42 <macrespo@student.42.fr>
2026-01-13 22:12:24 +0100

    feat(models): add family

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,15 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Family(models.Model):
+    name = models.CharField(max_length=42)
+
+
 class User(AbstractUser):
+    family = models.ForeignKey(
+        Family, on_delete=models.CASCADE, default=None, null=True, blank=True
+    )
+
     def __str__(self) -> str:
         return self.username
