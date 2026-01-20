1d9163e917173907cb63643eb6591d43ea145713
macrespo42 <macrespo@student.42.fr>
2026-01-20 19:02:21 +0100

    feat: init db schema

--- groceries/models.py
+++ groceries/models.py
@@ -2,11 +2,12 @@ from django.db.models.fields import DateTimeField
 from django.db.models.fields.related import ForeignKey
 from django.contrib.gis.db.models import CharField
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from accounts.models import Family
 
 
-class Family(models.Model):
+class Ingredients(models.Model):
     name: CharField = models.CharField(max_length=42)
+    category: CharField = models.CharField(max_length=42)
     created_at: DateTimeField = models.DateTimeField(auto_now_add=True)
     updated_at: DateTimeField = models.DateTimeField(auto_now=True)
 
@@ -14,12 +15,13 @@ class Family(models.Model):
         return self.name
 
 
-class User(AbstractUser):
-    family: ForeignKey[Family | None] = models.ForeignKey(
-        Family, on_delete=models.CASCADE, default=None, null=True, blank=True
+class GroceriesList(models.Model):
+    ingredients: ForeignKey[Ingredients] = models.ForeignKey(
+        to=Ingredients, on_delete=models.CASCADE
     )
+    family: ForeignKey[Family] = models.ForeignKey(to=Family, on_delete=models.CASCADE)
     created_at: DateTimeField = models.DateTimeField(auto_now_add=True)
     updated_at: DateTimeField = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.username
+        return f"Ingredients: {self.ingredients.name} in family: {self.family.name}"
