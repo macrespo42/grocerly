15dc4237a39fc7f780796b242bae5de8b018f6ae
macrespo42 <macrespo@student.42.fr>
2026-01-26 22:14:54 +0100

    feat(AUTH): make login EP

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 def index(request):
     return HttpResponse("Hello world. You are at the account index")
+
+
+class ProtectedHello(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {"message": "Hello from authenticated grocerly"}
+        return Response(content)
--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "accounts",
     "groceries",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 AUTH_USER_MODEL = "accounts.User"
 
